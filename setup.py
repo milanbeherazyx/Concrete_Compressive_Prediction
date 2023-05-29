da57f769dc00d081e89f1e6faecee3c9af62d5d6
milanbeherazyx <milanbeherazyx@mial.com>
2023-05-29 09:04:21 +0530

    update code

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -22,5 +23,4 @@ setup(
     author_email='milanbeherazyx@gmail.com',
     install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
-
 )
